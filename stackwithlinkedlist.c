2bd95ca452696c8888494a1859a4a94397c7b68a
Mohammed Ihsan <ihsanvp2738@gmail.com>
2022-03-10 13:14:52 +0530

    refactor stackwithlinkedlist

--- stackwithlinkedlist.c
+++ stackwithlinkedlist.c
@@ -1,111 +1,121 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
+typedef struct Node
+{
 	int data;
-	struct Node* link;
+	struct Node *link;
 } node;
 
-node* top = NULL; 
-node* temp = NULL;	
+node *top = NULL;
+node *temp = NULL;
 
 void push();
 void pop();
 void peek();
 void display();
 
-void main() {
-	
-	int run = 1, choice;
+void main()
+{
+	int choice;
 
-	do {	
-		printf("--------------------------\n");
-		printf("1. PUSH\n");
-		printf("2. POP\n");
-		printf("3. DISPLAY\n");
-		printf("4. PEEK\n");
-		printf("5. EXIT\n");
+	printf("STACK USING LINKED LIST\n");
+	printf("1.PUSH\n2.POP\n3.DISPLAY\n4.PEEK\n5.EXIT\n");
+
+	do
+	{
 		printf("Enter Choice [1/2/3/4/5] :");
 		scanf("%d", &choice);
 
-		switch(choice) {
-			case 1 :
-				printf("--------------------------\n");
-				push();
-				break;
-			case 2 :
-				printf("--------------------------\n");
-				pop();
-				break;
-			case 3 :
-				printf("--------------------------\n");
-				display();
-				break;
-			case 4 :
-				printf("--------------------------\n");
-				peek();
-				break;
-			case 5 :
-				run = 0;
-				break;
-			default :
-				printf("--------------------------\n");
-				printf("Error: Invalid Choice\n");		
-		}		
-	} while(run);
-				
+		switch (choice)
+		{
+		case 1:
+			push();
+			break;
+
+		case 2:
+			pop();
+			break;
+
+		case 3:
+			display();
+			break;
+
+		case 4:
+			peek();
+			break;
+
+		case 5:
+			break;
+
+		default:
+			printf("Error: Invalid Choice\n");
+		}
+	} while (choice != 5);
 }
 
-void push() {
+void push()
+{
 	int data;
 
 	printf("Enter data:");
 	scanf("%d", &data);
 
-	temp = (node*) malloc(sizeof(node));
+	temp = (node *)malloc(sizeof(node));
 	temp->data = data;
 	temp->link = NULL;
 
-	if (top != NULL) {
+	if (top != NULL)
+	{
 		temp->link = top;
 	}
 
 	top = temp;
-	
-	printf("Push: %d\n", data);
 }
 
-void pop() {
-	if (top == NULL) {
-		printf("Error: Stack Empty\n");
-	} else {
+void pop()
+{
+	if (top == NULL)
+	{
+		printf("UNDERFLOW\n");
+	}
+	else
+	{
 		temp = top;
 		top = top->link;
-		printf("Pop: %d\n", temp->data);
 		free(temp);
 	}
 }
 
-void peek() {
-	if (top == NULL) {
-		printf("Error: Stack Empty\n");
-	} else {
-		printf("Peek: %d\n", top->data);
+void peek()
+{
+	if (top == NULL)
+	{
+		printf("UNDERFLOW\n");
+	}
+	else
+	{
+		printf("Top Element : %d\n", top->data);
 	}
 }
 
-void display() {
-	if (top == NULL) {
-		printf("Error: Stack empty\n");
-	} else {
+void display()
+{
+	if (top == NULL)
+	{
+		printf("UNDERFLOW\n");
+	}
+	else
+	{
 		temp = top;
-		
-		printf("Stack: [");
-		while(temp != NULL) {
-			printf("%d,", temp->data);
+
+		printf("Stack: ");
+		while (temp != NULL)
+		{
+			printf("%d ", temp->data);
 			temp = temp->link;
 		}
-		
-		printf("]\n");
-	} 
+
+		printf("\n");
+	}
 }
