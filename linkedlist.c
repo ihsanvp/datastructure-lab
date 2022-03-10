eca5fe359353bc021e1c5c4b7d17922e13c7e8de
Mohammed Ihsan <ihsanvp2738@gmail.com>
2022-03-10 11:40:30 +0530

    fix linkedlist

--- linkedlist.c
+++ linkedlist.c
@@ -11,14 +11,27 @@ node *head = NULL;
 node *temp = NULL;
 node *new = NULL;
 node *ptr = NULL;
+node *parent = NULL;
 
 void insert_front();
 void insert_end();
-void insert_any();
+void insert_pos();
 void delete_front();
 void delete_end();
-void delete_any();
+void delete_pos();
 void display();
+
+node *createNode(int data)
+{
+    node *new = (node *)malloc(sizeof(node));
+    if (new != NULL)
+    {
+        new->data = data;
+        new->link = NULL;
+    }
+
+    return new;
+}
 
 void main()
 {
@@ -43,7 +56,7 @@ void main()
             break;
 
         case 3:
-            insert_any();
+            insert_pos();
             break;
 
         case 4:
@@ -55,7 +68,7 @@ void main()
             break;
 
         case 6:
-            delete_any();
+            delete_pos();
             break;
 
         case 7:
@@ -79,14 +92,12 @@ void insert_front()
     printf("Enter data :");
     scanf("%d", &item);
 
-    new = (node *)malloc(sizeof(node));
+    new = createNode(item);
 
     if (new != NULL)
     {
-        new->data = item;
         if (head == NULL)
         {
-            new->link = NULL;
             head = new;
         }
         else
@@ -108,14 +119,12 @@ void insert_end()
     printf("Enter data :");
     scanf("%d", &item);
 
-    new = (node *)malloc(sizeof(node));
+    new = createNode(item);
 
     if (new != NULL)
     {
-        new->data = item;
         if (head == NULL)
         {
-            new->link = NULL;
             head = new;
         }
         else
@@ -123,10 +132,10 @@ void insert_end()
             ptr = head;
             while (ptr != NULL)
             {
+                parent = ptr;
                 ptr = ptr->link;
             }
-            new->link = NULL;
-            ptr->link = new;
+            parent->link = new;
         }
     }
     else
@@ -134,7 +143,61 @@ void insert_end()
         printf("MEMORY UNDERFLOW\n");
     }
 }
-void insert_any() {}
+void insert_pos()
+{
+    int item, pos, i;
+
+    printf("Enter data :");
+    scanf("%d", &item);
+
+    printf("Enter position :");
+    scanf("%d", &pos);
+
+    new = createNode(item);
+
+    if (new != NULL)
+    {
+        if (head == NULL)
+        {
+            head = new;
+        }
+        else
+        {
+            ptr = head;
+            i = 0;
+            while (i < pos)
+            {
+                if (ptr->link == NULL)
+                {
+                    parent = ptr;
+                    ptr = NULL;
+                    break;
+                }
+                parent = ptr;
+                ptr = ptr->link;
+                i++;
+            }
+            if (ptr == head) // CASE 1 - first position
+            {
+                new->link = head;
+                head = new;
+            }
+            else if (ptr == NULL) // CASE 2 - last position
+            {
+                parent->link = new;
+            }
+            else // CASE 3 - middle position
+            {
+                new->link = parent->link;
+                parent->link = new;
+            }
+        }
+    }
+    else
+    {
+        printf("MEMORY UNDERFLOW\n");
+    }
+}
 
 void delete_front()
 {
@@ -151,20 +214,79 @@ void delete_front()
 }
 void delete_end()
 {
-    ptr = head;
-    while (ptr != NULL)
+    if (head != NULL)
     {
-        temp = ptr;
-        ptr = ptr->link;
-
-        if (ptr->link == NULL)
+        if (head->link == NULL)
         {
-            temp->link = NULL;
+            free(head);
+            head = NULL;
+        }
+        else
+        {
+            ptr = head;
+            while (ptr != NULL)
+            {
+                parent = ptr;
+                ptr = ptr->link;
+                if (ptr->link == NULL)
+                {
+                    break;
+                }
+            }
+            temp = parent->link;
+            parent->link = NULL;
+            free(temp);
+        }
+    }
+    else
+    {
+        printf("UNDERFLOW\n");
+    }
+}
+
+void delete_pos()
+{
+    int pos, i;
+    if (head == NULL)
+    {
+        printf("UNDERFLOW\n");
+    }
+    else
+    {
+        printf("Enter position to delete :");
+        scanf("%d", &pos);
+
+        ptr = head;
+        i = 0;
+        while (i < pos)
+        {
+            if (ptr->link == NULL)
+            {
+                printf("node not found at position '%d'\n", pos);
+                return;
+            }
+            parent = ptr;
+            ptr = ptr->link;
+            i++;
+        }
+
+        if (ptr == head) // CASE 1 - first position
+        {
+            head = ptr->link;
+            free(ptr);
+        }
+        else if (ptr->link == NULL) // CASE 2 - last position
+        {
+            parent->link = NULL;
+            free(ptr);
+        }
+        else // CASE 3 - middle position
+        {
+            parent->link = ptr->link;
             free(ptr);
         }
     }
 }
-void delete_any() {}
 
 void display()
 {
